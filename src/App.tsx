c725720c8e0cd41b374e78a8e6d1b8b876b144dd
Kunanon <non.somkham@gmail.com>
2023-10-01 00:35:34 +0700

    update search car and sort price

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useState, Fragment, ChangeEvent } from "react";
+import { useEffect, useState, Fragment, ChangeEvent, SetStateAction } from "react";
 import { useRecoilState } from "recoil";
-import { Dialog, Transition } from '@headlessui/react'
+import { Dialog, Transition, Menu } from '@headlessui/react'
 
 import { cartListState } from './store/recoil/atoms/common'
 
@@ -9,23 +9,32 @@ import { Car, CarInCart, DiscountCode } from "./types/common";
 import logo from "./images/logo.svg";
 import iconNullImage from "./images/icon-null-image.svg";
 import iconShopping from "./images/icon-shopping.svg";
-
+import iconDropdown from "./images/icon-dropdown.svg";
+import iconArrowDown from "./images/icon-arrow-down.svg";
+import iconClose from "./images/icon-close.svg";
 
 import "./index.css";
 import "./App.css";
+
+const menuList = [
+  { value: 'asc', name: 'Price: Low - High' },
+  { value: 'desc', name: 'Price: High - Low' },
+]
 
 const carListApi = 'https://cdn.contentful.com/spaces/vveq832fsd73/entries?content_type=car';
 const discountListApi = 'https://cdn.contentful.com/spaces/vveq832fsd73/entries?content_type=discount';
 
 const AccessToken = 'VPmo2U661gTnhMVx0pc0-CtahNg_aqS5DuneLtYfO1o';
 
-function App() {  
+function App() {
+  const [carListAll, setCarListAll] = useState<Car[]>([]);
   const [carList, setCarList] = useState<Car[]>([]);
   const [discountList, setDiscountList] = useState<DiscountCode[]>([]);
   const [isOpen, setIsOpen] = useState(false)
   const [total, setTotal] = useState(0)
   const [discount, setDiscount] = useState(0)
   const [grandTotal, setGrandTotal] = useState(0)
+  const [menuActive, setMenuActive] = useState({ value: 'asc', name: 'Price: Low - High' })
 
   const [cartList, setCartList] = useRecoilState<CarInCart[]>(cartListState);
 
@@ -33,7 +42,7 @@ function App() {
     let totalPrice = 0;
 
     cartList.forEach((carItem) => {
-      if (carItem.amountInCart && carItem.fields.price) {
+      if (carItem.amountInCart && carItem.fields.title) {
         totalPrice += carItem.amountInCart * carItem.fields.price;
       }
     });
@@ -43,6 +52,35 @@ function App() {
 
     setTotal(totalPrice);
     setGrandTotal(grandTotalChecked);
+  }
+
+  const handleSortPrice = (menu?: { value: string; name: string }) => {
+    const menuDefault = menu || menuActive
+    setMenuActive(menuDefault);
+
+    let sortedCarList = [...carList];
+
+    if (menuDefault.value === 'asc') {
+      sortedCarList = sortedCarList.sort((a, b) => a.fields.price - b.fields.price);
+    } else if (menuDefault.value === 'desc') {
+      sortedCarList = sortedCarList.sort((a, b) => b.fields.price - a.fields.price);
+    }
+
+    setCarList(sortedCarList);
+  }
+
+  const handleChangeSearchCar = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+
+    if (value) {
+      const filteredCarList = carListAll.filter((carItem) => {
+        const carTitle = carItem.fields.title.toLowerCase();
+        return carTitle.includes(value);
+      });
+      setCarList(filteredCarList)
+    } else {
+      setCarList(carListAll)
+    }
   }
 
   const handleChangeDiscountCode = (e: ChangeEvent<HTMLInputElement>) => {
@@ -112,7 +150,9 @@ function App() {
       }
 
       const data = await response.json();
-      setCarList(data.items)
+
+      setCarList(data.items);
+      setCarListAll(data.items);
     } catch (error) {
       console.error('error: ', error);
     }
@@ -152,6 +192,11 @@ function App() {
     }
   }, [cartList, isOpen, discount])
 
+  useEffect(() => {
+    handleSortPrice();
+  }, [carList.length])
+
+
   return (
     <div className="text-[#111827]">
       <div className="app-header shadow-lg w-full">
@@ -169,7 +214,39 @@ function App() {
       </div>
       <div className="flex justify-between px-10 py-3">
         <div className="text-3xl font-semibold">Car Available</div>
-        <div>Search</div>
+        <div className="flex gap-3">
+          <input
+            className="h-[43px] w-[250px] border px-5 border-[#D1D5DB] focus:outline-none rounded-md"
+            placeholder="Search Car"
+            onChange={(e) => handleChangeSearchCar(e)}
+          />
+          <Menu as="div" className="relative inline-block text-left">
+            <div>
+              <Menu.Button className="border flex items-center justify-center gap-2 h-[43px] px-5 border-[#D1D5DB] focus:outline-none rounded-md">
+                <img src={iconDropdown} alt="iconDropdown" />
+                {menuActive.name}
+                <img src={iconArrowDown} alt="iconArrowDown" />
+              </Menu.Button>
+            </div>
+            <Menu.Items className="absolute right-0 mt-2 w-full bg-white border border-[#D1D5DB] rounded-md shadow-lg focus:outline-none">
+              <div className="py-1">
+                {menuList.map((menu) => (
+                  <Menu.Item key={menu.value}>
+                    {({ active }) => (
+                      <button
+                        className={`${active ? 'bg-[#F3F4F6]' : ''
+                          } block px-4 py-2 text-sm text-gray-700 w-full text-left`}
+                        onClick={() => handleSortPrice(menu)}
+                      >
+                        {menu.name}
+                      </button>
+                    )}
+                  </Menu.Item>
+                ))}
+              </div>
+            </Menu.Items>
+          </Menu>
+        </div>
       </div>
       <div className="bg-[#F3F4F6] px-10 py-5">
         <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
@@ -216,7 +293,11 @@ function App() {
       </div>
 
       <Transition appear show={isOpen} as={Fragment}>
-        <Dialog as="div" className="relative z-10" onClose={() => setIsOpen(false)}>
+        <Dialog
+          as="div"
+          className="relative z-10"
+          onClose={() => setIsOpen(false)}
+        >
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
@@ -240,12 +321,18 @@ function App() {
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel className="w-full max-w-xl transform overflow-hidden rounded-xl bg-white text-left py-4 px-5 transition-all">
+                <Dialog.Panel className="relative w-full max-w-xl transform overflow-hidden rounded-xl bg-white text-left py-4 px-5 transition-all">
                   <Dialog.Title
                     as="h3"
                     className="text-3xl font-semibold"
                   >
                     Cart
+                    <img
+                      className="absolute top-5 right-5 cursor-pointer"
+                      src={iconClose}
+                      alt="iconClose"
+                      onClick={() => setIsOpen(false)}
+                    />
                   </Dialog.Title>
                   <div className="flex flex-col justify-between">
                     <div className="mt-6 flex flex-col gap-3 h-[250px] overflow-auto">
